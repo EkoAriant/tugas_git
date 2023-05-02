4fa875edea9dc076bd34db22b1cfb20186506e2e
EkoAriant <132032996+EkoAriant@users.noreply.github.com>
2023-05-02 08:59:18 +0700

    Create tampil_nama.php
    berisi kode php untuk menampilkan nama

---
+++ tampil_nama.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Tugas Display PHP</title>
+        <!--Created By : Eko Yuli Arianto-->
+    </head>
+<body>
+ <?php
+    $nama="Eko Yuli Arianto";
+  
+    echo "Tugas Github 2 Tampil Nama";
+    echo "<br>";
+    echo "Nama saya adalah : $nama";
+    ?>
+</body>
+</html>
